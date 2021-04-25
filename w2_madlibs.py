b9c24e72205d8c4dab6ab66c294c0a46854286cc
Michael Wiseman <mcwiseman97@gmail.com>
2021-04-25 11:38:40 -0700

    Building Block Assignments

---
+++ w2_madlibs.py
@@ -0,0 +1,15 @@
+print("\n")
+print("Please enter the following: ")
+print("\n")
+
+adj = input("Adjectve: ")
+animal = input("Animal: ")
+verbOne = input("Verb One: ")
+verbTwo = input("Verb Two: ")
+verbThree = input("Verb Three: ")
+exclamation = input("Exclamation: ")
+
+
+print("\n")
+print("The other day, I was really in trouble. It all started when I saw a very " + adj + " " + animal + " " + verbOne + " down the hallway. \n" + exclamation + "! I yelled. But all I could think to do was to " + verbTwo + " over and over. Miraculously, that caused it to stop, \nbut not before it tried to " + verbThree + " right in front of my family.")
+print("\n")
