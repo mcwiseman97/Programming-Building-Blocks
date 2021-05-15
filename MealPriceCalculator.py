904570c4d59d62f874b542ecdca1d3906a109fdf
Michael Wiseman <mcwiseman97@gmail.com>
2021-05-15 15:52:09 -0700

    Week 4 Assignment
    Meal Calculator

---
+++ MealPriceCalculator.py
@@ -0,0 +1,14 @@
+child_meal = input(" What is the price of a child's meal? ")
+adult_meal = input("What is the price of an adult's name? ")
+num_child = input("How many children are there? ")
+num_adult = input("How many adults are there? ")
+tax_rate = input("What is the sales tax rate? ")
+total = 
+print("")
+print("Subtotal: ")
+print("Sales Tax: ")
+print("Total: ")
+print("")
+pay_amount = input("What is the payment amount? ")
+change = pay_amount - total
+print("Change: ")
