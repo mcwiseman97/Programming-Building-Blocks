1fc435d01fed1af84091288b1618e3109bfe12ea
Michael Wiseman <mcwiseman97@gmail.com>
2021-07-10 12:12:03 -0700

    Uploaded week 11 assignment

---
+++ Week11Milestone.py
@@ -0,0 +1,15 @@
+print()
+
+year = int(input("Enter the year of preference: "))
+maxpercentage = 86.751
+minpercentage = 17.76
+max_year = 2019
+min_year = 1882
+print()
+maxplace = "monaco"
+minplace = "Iceland"
+print("The overall max life expectancy is:", maxpercentage, "from", maxplace, "in,", max_year )
+print("The overall min life expectancy is:", minpercentage, "from", minplace, "in,", min_year )
+print()
+print("For the year ", year)
+
