2c9ed80717e9da72f17920880cbfa1c727b335c5
Michael Wiseman <mcwiseman97@gmail.com>
2021-07-10 17:28:03 -0700

    added csv

--- Week11Milestone.py
+++ Week11Milestone.py
@@ -1,3 +1,10 @@
+import csv
+
+with open("life-expectancy.csv", mode='r')as file:
+    csvFile = csv.reader(file)
+    for lines in csvFile:
+        print(lines)
+
 print()
 
 year = int(input("Enter the year of preference: "))
@@ -6,10 +13,3 @@ minpercentage = 17.76
 max_year = 2019
 min_year = 1882
 print()
-maxplace = "monaco"
-minplace = "Iceland"
-print("The overall max life expectancy is:", maxpercentage, "from", maxplace, "in,", max_year )
-print("The overall min life expectancy is:", minpercentage, "from", minplace, "in,", min_year )
-print()
-print("For the year ", year)
-
